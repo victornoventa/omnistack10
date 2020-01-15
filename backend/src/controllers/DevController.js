075d8a060a2a8c67afc8c7fc4b58209d30e4bf37
Victor Noventa <victor.noventa@avenuecode.com>
2020-01-14 22:00:29 -0300

    Add GET /devs, POST /devs and GET /search endpoints

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    list: async (request, response) => {
+        const devs = await Dev.find();
+        return response.json(devs);
+    },
+    store: async (request, response) => {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        const foundDev = await Dev.findOne({ github_username });
+        if (foundDev) {
+            return response.json(foundDev);
+        }
+
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+        const { name, login, avatar_url, bio } = apiResponse.data;
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        };
+
+        const dev = await Dev.create({
+            github_username,
+            name: name ? name : login,
+            avatar_url,
+            bio,
+            techs,
+            location
+        });
+
+        return response.json(dev);
+    }
+};
